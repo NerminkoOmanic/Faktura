9ce362cce92938e52176cbfa39fb89b445cecb23
NerminkoOmanic <omanicnerminko@gmail.com>
2020-11-13 17:06:04 +0100

    AreaRadnik_HomeIndex_PorezIndex

---
+++ Faktura/Faktura/Areas/Radnik/Controllers/PorezController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Faktura.Areas.Radnik.Models;
+using Faktura.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Faktura.Areas.Radnik.Controllers
+{
+    [Area("Radnik")]
+    public class PorezController : Controller
+    {
+        
+        private FakturaDbContext _db;
+
+        public PorezController(FakturaDbContext context)
+        {
+            _db = context;
+        }
+
+        public IActionResult Index()
+        {
+            PorezListVM model = new PorezListVM()
+            {
+                Rows = _db.Pdv.Select(x => new PorezListVM.Row()
+                {
+                    PdvId = x.PdvId,
+                    Drzava = x.Drzava,
+                    Iznos = (int)(x.IznosPdv * 100)
+
+                }).ToList()
+            };
+            return View(model);
+        }
+    }
+}
---
+++ Faktura/Faktura/Areas/Radnik/Models/PorezListVM.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Faktura.Areas.Radnik.Models
+{
+    public class PorezListVM
+    {
+        public List<Row> Rows { get; set; }
+
+        public class Row
+        {
+            public int PdvId {get; set; }
+            public string Drzava {get; set; }
+            public int Iznos {get; set; }
+        }
+    }
+}
